c10d2a63e34d8215ed3fdcdcb206bfbd9e52f4a6
zaramarie <47329894+zaramarie@users.noreply.github.com>
2019-02-07 11:02:03 -0600

    demo 1 (feb7th)

--- 01/cs30-p5js-template-master/sketch.js
+++ 01/cs30-p5js-template-master/sketch.js
@@ -4,12 +4,21 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+let X = 45;
+let XSpeed =6;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER)
 }
 
 function draw() {
-  background(220);
+  background(0);
+  fill(255,204,0);
+  ellipse(X,200, 200);
+  fill(50);
+  rect(mouseX,400,75,75);
+  X += XSpeed;
+  if (X > windowHeight) X =30
 }
