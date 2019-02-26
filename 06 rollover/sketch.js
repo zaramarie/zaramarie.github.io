e9c77c5df2a7d950f2505f6c4aa502ef05a11191
zaramarie <47329894+zaramarie@users.noreply.github.com>
2019-02-26 11:01:32 -0600

    makin rectangles

--- 06 rollover/sketch.js
+++ 06 rollover/sketch.js
@@ -5,11 +5,30 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let quad1 = 0;
+let quad2 = 0;
+let quad3 = 0;
+let quad4 = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
-  background(220);
+  background(255);
+
+  rectMode(CENTER)
+  
+  //q1
+  rect((windowWidth/2) + 100, (windowHeight/2) - 100, 200, 200);
+  //q2
+  rect((windowWidth/2) - 100, (windowHeight/2) - 100, 200, 200 );
+  //q3
+  rect((windowWidth/2) - 100, (windowHeight/2) + 100, 200, 200);
+  //q4
+  rect((windowWidth/2) + 100, (windowHeight/2) + 100, 200, 200);
+
+
+  
 }
