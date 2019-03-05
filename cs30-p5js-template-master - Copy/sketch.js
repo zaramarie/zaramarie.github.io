ce8b7018943b1d60e191f41df8d162be93c32215
zaramarie <47329894+zaramarie@users.noreply.github.com>
2019-03-05 10:08:11 -0600

    demo

---
+++ cs30-p5js-template-master - Copy/sketch.js
@@ -0,0 +1,18 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let button;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  button  = createButton("test");
+  button.position(30,30);
+}
+
+function draw() {
+  background(220);
+}
