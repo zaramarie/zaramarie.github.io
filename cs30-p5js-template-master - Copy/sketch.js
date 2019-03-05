0188ea35a686fb2eb4fce773cae39990c9818761
zaramarie <47329894+zaramarie@users.noreply.github.com>
2019-03-05 11:03:10 -0600

    uwu

--- cs30-p5js-template-master - Copy/sketch.js
+++ cs30-p5js-template-master - Copy/sketch.js
@@ -6,17 +6,50 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let slider;
+let red;
+let green;
+let blue;
+let radio;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  slider = createSlider(0, 255, 120);
-  slider.position(10, 20);
-  slider.style("width", "180px");
+  red = createSlider(0, 255, 120);
+  red.position(width/2, height/2);
+  red.style("width", "180px");
+
+  green = createSlider(0, 255, 120);
+  green.position(width/2, (height/2) + 40);
+  green.style("width", "180px");
+
+  blue = createSlider(0, 255, 120);
+  blue.position(width/2, (height/2) + 80);
+  blue.style("width", "180px");
+
+  radio = createRadio();
+  radio.option('ellipse', 1);
+  radio.option('square', 2);
+  radio.style('width', '60px');
+  textAlign(CENTER);
+  radio.position((width/2), (height/2) - 100);
 }
 
 function draw() {
-  background(slider.value());
-  print(slider.value());
-  text(slider.value(), 20, 50);
+  background(red.value(), green.value(), blue.value());
+  
+  print(red.value());
+  text(red.value(), width/2, height/2 + 10);
+
+  print(green.value());
+  text(green.value(), width/2, height/2 + 50);
+  
+  print(blue.value());
+  text(blue.value(), width/2, height/2 + 90);
+
+  if (radio.value() === '1'){
+    ellipse(width/4, height/2, 100, 100);
+  }
+
+  else if (radio.value() === '2'){
+    rect(width/4, height/2, 100, 100);
+  }
 }
