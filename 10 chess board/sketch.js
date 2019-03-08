70e10ce215305d0cf9a41cd9b60ba0d9634f2918
Goldney2101207 <2101207@sps-student.sk.ca>
2019-03-08 10:04:52 -0600

    :p

--- 10 chess board/sketch.js
+++ 10 chess board/sketch.js
@@ -13,10 +13,5 @@ function setup() {
 
 function draw() {
   background(255);
-  let s = height/8;
-  for( x = 0; x <= width;x = x + s){
-		for(y = 0;y <= height; y = y + s){
-		    rect( x, y, s, s);
-		}
-	}
+  
 }
