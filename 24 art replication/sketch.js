3202f4f37813e49c4d3b1ea94258f2111609ef94
Goldney2101207 <2101207@sps-student.sk.ca>
2019-04-11 11:02:01 -0600

    replication

--- 24 art replication/sketch.js
+++ 24 art replication/sketch.js
@@ -8,8 +8,19 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255);
+  fill(240);
+  noStroke();
+  ellipse(width/2, height/2, 500, 500);
+  rectangles();
 }
 
-function draw() {
-  background(220);
+function rectangles(){
+  
+  for ( let i = 0; i < 300; i++){
+    fill(0);
+    rect(random(width/2 + 225, width/2 -225), random(height/2 + 225, height/2 -225) , random(1, 20), random(1, 20));
+  }
+
+  
 }
